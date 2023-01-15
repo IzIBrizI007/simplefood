a2bdb3b39b34a37d8a55609c40baa2b1c251c67c
IzIBrizI007 <anatolij.goncharenko90@gmail.com>
2023-01-15 17:30:38 +0200

    problem with svg

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,39 @@
 $(function () {
   $('.comments__slider').slick({
     dots: true,
-    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="lnr lnr-chevron-left"></span></button>',
+    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="lnr lnr-chevron-right"></span></button>',
+    appendArrows: '.arrows-wrap'
   });
+
 });
+
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+  .pipe(cheerio({
+        run: ($) => {
+            $("[fill]").removeAttr("fill"); 
+            $("[stroke]").removeAttr("stroke"); 
+            $("[style]").removeAttr("style"); 
+        },
+        parserOptions: { xmlMode: true },
+      })
+  )
+	.pipe(replace('&gt;','>')) // боремся с заменой символа 
+	.pipe(
+	      svgSprite({
+	        mode: {
+	          stack: {
+	            sprite: '../sprite.svg', 
+	          },
+	        },
+	      })
+	    )
+	.pipe(dest('app/images')); 
+}
 
 var mixer = mixitup('.categories__choice');
 mixer.filter('.category-a');
