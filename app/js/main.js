59144ee5602c66ea0dbce74a97eb442e05a30bc4
IzIBrizI007 <anatolij.goncharenko90@gmail.com>
2023-01-18 23:48:41 +0200

    added adaptive version and devided html file

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,53 @@
 $(function () {
-  $('.comments__slider').slick({
-    dots: true,
-    autoplay: true,
-    autoplaySpeed: 5000,
-    fade: true,
-    prevArrow: '<button type="button" class="slider__arrow slider__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
-    nextArrow: '<button type="button" class="slider__arrow slider__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
-    appendArrows: '.arrows-wrap'
-  });
 
-  function svgSprites() {
-  return src('app/images/icons/*.svg') 
-  .pipe(cheerio({
-        run: ($) => {
-            $("[fill]").removeAttr("fill"); 
-            $("[stroke]").removeAttr("stroke"); 
-            $("[style]").removeAttr("style"); 
-        },
-        parserOptions: { xmlMode: true },
-      })
-  )
-	.pipe(replace('&gt;','>')) // боремся с заменой символа 
-	.pipe(
-	      svgSprite({
-	        mode: {
-	          stack: {
-	            sprite: '../sprite.svg', 
-	          },
-	        },
-	      })
-	    )
-	.pipe(dest('app/images')); 
-}
+  $('.burger').on('click', function(){
+  $('.burger').toggleClass('burger__line--active');
+  
+
+  $('body').toggleClass('lock');
+  $('.burger-nav').toggleClass('burger-nav--active');
+});
+
+  if($(window).width() > 577)
+    {   
+      $('.comments__slider').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // fade: true, 
+        infinite: false,
+        prevArrow: '<button type="button" class="comments__arrow comments__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+        nextArrow: '<button type="button" class="comments__arrow comments__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+        appendArrows: '.comments__slider'
+      });
+    }
+
+    else {
+      $('.comments__slider').slick({
+        dots: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        // fade: true,
+        infinite: false,
+        prevArrow: '<button type="button" class="comments__arrow comments__arrow--prev"><span class="sr-only">Предидущий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+        nextArrow: '<button type="button" class="comments__arrow comments__arrow--next"><span class="sr-only">Следующий слайд</span><svg class="icons-sprite"><use xlink:href="images/sprite.svg#icon-vector"></use></svg></button>',
+        appendArrows: '.comments__slider'
+      });
+    }
+
+  if($(window).width() < 577)
+    {   
+      $('.restorans__list').slick({
+        dots: true,
+        arrows : false,
+        autoplay: true,
+        autoplaySpeed: 11111000,
+      });
+    }
 
 });
 
 
 
-var mixer = mixitup('.categories__choice');
+var mixer = mixitup('.choice__foods');
 mixer.filter('.category-a');
