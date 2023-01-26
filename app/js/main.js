770c5c7066efb1963979cca9b2b9e51480dcafc9
IzIBrizI007 <anatolij.goncharenko90@gmail.com>
2023-01-26 23:46:59 +0200

    added page catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,55 @@
 $(function () {
 
+  $('.select-styler').styler();
+
+
   $('.burger').on('click', function(){
   $('.burger').toggleClass('burger__line--active');
   
   $('body').toggleClass('lock');
   $('.burger-nav').toggleClass('burger-nav--active');
+  });
 
-  $(document).mouseup( function(e){
+$('.burger-filter').on('click', function(){
+  $('.burger-filter').toggleClass('burger-filter--active');
+  $('.catalog__filter-box').toggleClass('catalog__filter-box--active');
+  $('body').toggleClass('lock');
+});
+
+$(document).mouseup( function(e){  
   var div = $('.burger'); 
-		if (!$('.burger-nav').is(e.target)
-		    && $('.burger-nav').has(e.target).length === 0 ) { 
+		if (!$('.burger-nav, .catalog__filter-box').is(e.target)
+		    && $('.burger-nav, .catalog__filter-box').has(e.target).length === 0 ) { 
       $('.burger').removeClass('burger__line--active');
+      $('.burger-filter').removeClass('burger-filter--active');
 			$('.burger-nav').removeClass('burger-nav--active');
+			$('.catalog__filter-box').removeClass('catalog__filter-box--active');
       $('body').removeClass('lock'); 
 		}
-	});
+});
+
+$('.filter-category__btn').on('click', function(){
+  $('.burger-filter').removeClass('burger-filter--active');
+  $('.catalog__filter-box').removeClass('catalog__filter-box--active');
+  $('body').removeClass('lock');
 });
 
 
     $(window).on('load resize', function() {
-      if ($(window).width() < 561) {
-        $('.restorans__list:not(.slick-initialized)').slick({
+      if ($(window).width() < 769) {
+        $('.restorans__list:not(.slick-initialized), .discount__list:not(.slick-initialized)').slick({
           dots: true,
           arrows : false,
           autoplay: true,
-          autoplaySpeed: 11111000,
+          autoplaySpeed: 12000,
         });
-      } else {
+      } if ($(window).width() > 769) {
         $(".restorans__list.slick-initialized").slick("unslick");
+        $(".discount__list.slick-initialized").slick("unslick");
       }
     });
+
+
 
     $('.comments__slider:not(.slick-initialized)').slick({
       dots: true,
@@ -51,5 +71,65 @@ $(function () {
     });
 });
 
-var mixer = mixitup('.choice__foods');
+    var $range = $(".filter-price__input");
+    var $inputFrom = $(".filter-price__from");
+    var $inputTo = $(".filter-price__to");
+    var instance;
+    var min = 0;
+    var max = $inputTo;
+    var from = 0;
+    var to = 0;
+    
+    $range.ionRangeSlider({
+        type: "double",
+        onStart: updateInputs,
+        onChange: updateInputs,
+        onFinish: updateInputs
+    });
+    instance = $range.data("ionRangeSlider");
+    
+    function updateInputs (data) {
+        from = data.from;
+        to = data.to;
+    
+        $inputFrom.prop("value", from);
+        $inputTo.prop("value", to);
+    }
+    
+    $inputFrom.on("change", function () {
+        var val = $(this).prop("value");
+    
+        // validate
+        if (val < min) {
+            val = min;
+        } else if (val > to) {
+            val = to;
+        }
+    
+        instance.update({
+            from: val
+        });
+    
+        $(this).prop("value", val);
+    
+    });
+    
+    $inputTo.on("change", function () {
+        var val = $(this).prop("value");
+    
+        // validate
+        if (val < from) {
+            val = from;
+        } else if (val > max) {
+            val = max;
+        }
+    
+        instance.update({
+            to: val
+        });
+    
+        $(this).prop("value", val);
+    });
+
+var mixer = mixitup('.choice__foods, .catalog__list');
 mixer.filter('.category-a');
